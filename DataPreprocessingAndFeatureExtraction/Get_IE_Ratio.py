78b1733fad709ed5d3cb774ebd09efa51520380e
pallavig702 <31351096+pallavig702@users.noreply.github.com>
2024-05-16 16:32:40 -0500

    Update Get_IE_Ratio.py

--- DataPreprocessingAndFeatureExtraction/Get_IE_Ratio.py
+++ DataPreprocessingAndFeatureExtraction/Get_IE_Ratio.py
@@ -1,27 +1,19 @@
-import numpy as np
-import statistics
-import pandas as pd
-#from math import nan
-#import statsmodels as sm
-#from patsy import dmatrices
-#from statsmodels.sandbox.tsa import movmean
 import math
-
+import statistics
+import numpy as np
+import pandas as pd
 
 
 def Calculate_IE_Ratio(data):
-    #data.to_csv("ChcekingIE.csv")
-    #exit(0)
     data['resp_pv_dt_IE'] = pd.to_datetime(data['resp_pv_dt_IE'])
     i=0
     Resp_Cycle=1
-    #print()
-
+    
     r_pvs = data['resp_pvs'].to_list()
     r_vals = data['resp_pv_vals'].to_list()
     r_hts = data['resp_pv_hghts'].to_list()
     r_time = data['resp_pv_dt_IE'].to_list()
-    #print(len(r_pvs),len(r_vals),len(r_hts),len(r_time))
+    
     I_E=[]
     Start=[]
     End=[]
@@ -49,13 +41,12 @@ def Calculate_IE_Ratio(data):
             I=(r_time[i+1]-r_time[i]).total_seconds()
             E=(r_time[i+2]-r_time[i+1]).total_seconds() # Skipped milli seconds because a lag is expected
             if((I==0) and (E==0)):
-                    continue
-                    
+                    continue        
             
             if numo==0:
-                print("HAHHAHAHAHAHHAHAHAHHAHAHAH")
+                print("From Get IE Ratio Script")
                 print(r_time[i])
-                print("HAHHAHAHAHAHHAHAHAHHAHAHAH")
+                print("From Get IE Ratio Script")
             numo=numo+1
             
             I_E.append(round((I/E),1))
@@ -81,14 +72,9 @@ def Calculate_IE_Ratio(data):
             count.append(1) #count of normal peaks exclusing apnea peaks with label 0
         else:
             i=i+1
-    #d = {'Start':Start,'End':End,'I_E':I_E}
-    #d = {'Start':Start,'End':End,'I_E':I_E,'I_only':I_only,'E_only':E_only,\
-     #       'I_by_Total':I_by_Total,'Total_IE':Total_IE,'Total_IE2':Total_IE2,'Diff_IE':Diff_IE,"count":count}
-    
+  
     d = {'Start':Start,'End':End,'I_E':I_E,'I_only':I_only,'E_only':E_only,
          'I_by_Total':I_by_Total,'Total_IE':Total_IE,'Total_IE2':Total_IE2,'Diff_IE':Diff_IE,"count":count,"Insp_diff_in_amplitudes":Insp_diff_in_amplitudes,"Exp_diff_in_amplitudes":Exp_diff_in_amplitudes,"ht":ht,"Diff_In_Valley_amplitudes":Diff_In_Valley_amplitudes}
     df = pd.DataFrame(d)
-    #print(df.dtypes)
-    #df.head()
 
     return(df)
