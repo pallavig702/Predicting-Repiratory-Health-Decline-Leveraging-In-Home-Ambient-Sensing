5b7615fadd9501a68a41504fdde7eee9ebd9d55b
pallavig702 <31351096+pallavig702@users.noreply.github.com>
2024-10-13 17:52:31 -0500

    Update and rename Perform_BIRCH_Clustering.py to Run_BIRCH_Clustering.py

---
+++ Run_BIRCH_Clustering.py
@@ -0,0 +1,157 @@
+import time
+import os, sys
+import pandas as pd
+import numpy as np
+import matplotlib
+#from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as shc
+
+# Packages related to BIRCH
+from numpy import where
+from numpy import unique
+from sklearn.cluster import Birch
+from sklearn.datasets import make_classification
+from sklearn.datasets.samples_generator import make_blobs
+plt.rcParams.update({'font.size': 22})
+
+# Packages related to TSNE Projections
+import matplotlib
+import seaborn as sns
+from sklearn.manifold import TSNE
+
+
+############################################################################################
+############### import the features extracted fron Run_DataMergeAndMining.py script ########
+############################################################################################
+
+df_Comb=pd.read_csv('ExtractedFeatureFile.csv')
+
+############################################################################################
+########################### Dendogram for Heirarchical Clustering ##########################
+####### Dendrogram building helps in identifying the num_clusters hyperparameters 
+################### for hierarchial clustering algorithms
+############################################################################################
+
+# To extract for specific ID
+i_dates='3054'#'3083'#
+df_3054_1=df_Comb[df_Comb['ID']==i_dates]
+
+
+IDdd=df_3054_1['ID'].to_list()
+Index=df_3054_1['Index'].to_list()
+dates=df_3054_1['Date'].to_list()
+print(np.unique(dates))
+df_3054_Clust=df_3054_1.drop(columns=['ID','Date','Index'])
+df_3054_Clust.head()
+
+pyplot.figure(figsize=(10, 7))  
+pyplot.title("Dendrograms")  
+matplotlib.rcParams.update({'font.size': 22})
+
+#This generates a dendrogram visualization
+dend = shc.dendrogram(shc.linkage(df_3054_Clust, method='ward'))
+
+
+###########################################################################################
+############################# BIRCH Clustering Function ###################################
+###########################################################################################
+
+# define dataset
+
+def BirchClustering(X,n):
+    model = Birch(threshold=0.01, n_clusters=n)
+   
+    # fit the model
+    model.fit(X)
+    # assign a cluster to each example
+    yhat = model.predict(X)
+    # retrieve unique clusters
+    clusters = unique(yhat)
+    # create scatter plot for samples from each cluster
+    X, clusters = make_blobs(n_samples=X.shape[0], centers=len(clusters), cluster_std=0.70, random_state=0)
+    plt.scatter(X[:,0], X[:,1], alpha=0.7, edgecolors='b')
+    plt.scatter(X[:,0], X[:,1], c=clusters, cmap='rainbow', alpha=0.7)#, edgecolors='b')
+    return(yhat)
+
+
+
+df_3054_1=df_Comb[df_Comb['ID']==i_dates]
+IDdd=df_3054_1['ID'].to_list()
+Index=df_3054_1['Index'].to_list()
+dates=df_3054_1['Date'].to_list()
+print(np.unique(dates))
+df_3054_Clust=df_3054_1.drop(columns=['ID','Date','Index'])
+display(df_3054_Clust.head())
+
+Label=BirchClustering(df_3054_Clust,6) #7 #With Noise(7), Without Noise(6)
+display(df_3054_Clust.head())
+
+#Counter
+print(Counter(Label))
+
+label=shuffleLabelAsPerDescRespRate(df_3054_Clust,Label)
+display(df_3054_Clust.head())
+
+Unique_Labels=list((Counter(label)).keys())
+print(sorted(Counter(label).items()))
+
+label_df=pd.DataFrame()
+label_df=pd.DataFrame.from_dict(Counter(label), orient='index').T
+label_df['date']=i_dates
+
+label_df.to_csv(LabelCount, mode='a', index=False, header=False)
+
+
+
+#################################################################################################
+##### BUILD 2-D TSNE PROJECTIONS OF HIGH DIMENSIONAL DATA colored by cluster category ###########
+#################################################################################################
+
+####################### Functions for building TSNE #######################
+def CallTNSE2(data, Label,titleName,date,path,alpha):#,Res_ID):
+    #Source: https://www.datatechnotes.com/2020/11/tsne-visualization-example-in-python.html
+    #Source: https://seaborn.pydata.org/generated/seaborn.scatterplot.html
+    #STEP 1: TRANSFORM DATA
+    data=data.drop(columns=['label'])
+    display(data.head())
+    tsne = TSNE(n_components=2, verbose=1, random_state=123,perplexity=120)#,early_exaggeration=40.0) #perplexity=50,
+    z = tsne.fit_transform(data)
+    
+    #perplexity=100 this worked,#early_exaggeration=40.0
+    display(z)
+    #STEP 2: PROJECT MULTIDIMENSIONAL DATA IN 2D
+    df = pd.DataFrame()
+    df["y"] = Label
+    #df["id"]=Res_ID
+    df["comp-1"] = z[:,0]
+    df["comp-2"] = z[:,1]
+    
+    matplotlib.rcParams.update({'font.size': 16})
+
+    c=Counter(Label)
+    num_clust=len(c.keys())
+    #color_dict = dict({'COPD':'brown','Non-COPD':'green'})
+    sns_plot = sns.scatterplot(x="comp-1", y="comp-2", hue=df.y.tolist(),
+                    palette=sns.color_palette("hls", num_clust),\
+                    data=df,alpha=alpha)
+    #palette=color_dict
+
+    #ADD NUMBERS TO PLOT
+    for label in df.y.unique():
+    # randomly sample
+        tmp = df.loc[df['y']==label].sample(1)
+        #add label to some random points per group
+        for _,row in tmp.iterrows():
+            plt.annotate(label, (row['comp-1'], row['comp-2']), size=14, weight='bold', color='k')
+     
+    
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    #sns_plot.legend(loc='upper left', bbox_to_anchor=(1, 1), ncol=1)
+    #sns.move_legend(sns_plot, "upper left", bbox_to_anchor=(1, 1))
+    plt.savefig(path+"/"+date+"_"+titleName+".png") 
+    return()
+
+####################### Call For BULDING TSNE #######################
+alpha=1
+CallTNSE2(df_3054_Clust, label, "Clustered data",i_dates,Resultpath,alpha)
